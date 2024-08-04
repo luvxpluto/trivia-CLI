e6134bd4862d91ef02c0156ecca7f6b757a35bee
luvxpluto <estebansnz1226@icloud.com>
2024-08-03 21:35:42 -0600

    feat: define functions and structures for player, node and players list

---
+++ players.h
@@ -0,0 +1,38 @@
+//
+// Created by Luvxpluto on 8/3/24.
+//
+
+#ifndef TRIVIA_CLI_PLAYERS_H
+#define TRIVIA_CLI_PLAYERS_H
+
+typedef struct Node Node;
+typedef struct Player Player;
+typedef struct PlayersList PlayersList;
+
+struct PlayersList{
+    Node *start;
+};
+
+struct Player{
+    char* id;
+    char* name;
+    char* nickname;
+    char* mail;
+};
+
+struct Node{
+    Player Player;
+    Node *next;
+};
+
+void addPlayer(PlayersList *players, Player p);
+void viewPlayers(PlayersList *players);
+void viewPlayer(PlayersList *players);
+void deletePlayer(PlayersList *players);
+void updateName(PlayersList *players);
+void updateNickname(PlayersList *players);
+void updateMail(PlayersList *players);
+Player getPlayerData();
+void createPlayer(PlayersList *players, Player p);
+void freePlayerList(PlayersList * player);
+#endif //TRIVIA_CLI_PLAYERS_H
