ec5ad3982f7211b3d8b0ece3cae5bb4907276227
luvxpluto <estebansnz1226@icloud.com>
2024-08-16 19:12:03 -0600

    feat: add questions files

---
+++ questions.c
@@ -0,0 +1,5 @@
+//
+// Created by Luvxpluto on 8/16/24.
+//
+
+#include "questions.h"
---
+++ questions.h
@@ -0,0 +1,8 @@
+//
+// Created by Luvxpluto on 8/16/24.
+//
+
+#ifndef TRIVIA_CLI_QUESTIONS_H
+#define TRIVIA_CLI_QUESTIONS_H
+
+#endif //TRIVIA_CLI_QUESTIONS_H
